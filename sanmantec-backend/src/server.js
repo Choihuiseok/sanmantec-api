aca629b6ccf05ba0a0912c188a20de67ce42f4ef
Choi Huiseok <noanlove1@gmail.com>
2025-11-22 22:13:30 +0900

    백엔드 날림

--- sanmantec-backend/src/server.js
+++
@@ -1,8 +0,0 @@
-require("dotenv").config();
-const app = require("./app");
-
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, () => {
-  console.log(`⚡ Sanmantec Backend running on port ${PORT}`);
-});
